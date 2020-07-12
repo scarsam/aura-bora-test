7bfde8c346cdfeeb7273f81888180ee8554376ae
sam <s.ojlin@gmail.com>
2020-07-12 18:35:33 +0200

    Fixed react console errors

--- src/components/where-to-find/Modal.js
+++ src/components/where-to-find/Modal.js
@@ -9,7 +9,7 @@ const Modal = ({ state, cities, handleCloseModal, innerModalRef }) => {
   const renderSelectedCity = () => {
     const city = cities.find(({ city }) => city === selectedCity)
     return city?.stores.map(({ name, address }) => (
-      <Store name={name} address={address} />
+      <Store key={name} name={name} address={address} />
     ))
   }
 
@@ -23,8 +23,8 @@ const Modal = ({ state, cities, handleCloseModal, innerModalRef }) => {
     }
 
     return cities.map(({ city }) => (
-      <li onClick={() => setSelectedCity(city)}>
-        <City key={city} city={city} />
+      <li key={city} onClick={() => setSelectedCity(city)}>
+        <City city={city} />
       </li>
     ))
   }
@@ -36,7 +36,7 @@ const Modal = ({ state, cities, handleCloseModal, innerModalRef }) => {
           <div className="row padding-bottom-30px">
             <div
               ref={innerModalRef}
-              className="col-12 col-md-10 offset-md-1 padding-bottom-60px"
+              className="col-12 col-md-10 offset-md-1 margin-bottom-60px"
             >
               <section className="padding-none d-flex justify-content-center align-items-center text-center modal-title z-index-1 padding-left-60px padding-right-50px">
                 <h2 className="margin-bottom-none text-30px absolute">
