05aefd086e9a2d0b14824aabb36812656c1c0b2e
Maximilian <max-dz@hotmail.com>
2020-08-03 08:39:43 +0200

    Remove Image-component import

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,7 +3,7 @@ import ReactMarkdown from 'react-markdown'
 import CarouselImage from './CarouselImage'
 import ArrowBack from 'images/about/carousel/arrow-prev.svg'
 import ArrowForward from 'images/about/carousel/arrow-next.svg'
-import Image from '../components/image'
+
 import {
   CarouselProvider,
   Slider,
@@ -33,56 +33,54 @@ const Carousel = ({ testamonials }) => {
     }
   }, [])
   return (
-    <>
-      <div className="container padding-top-40px padding-top-lg-60px padding-bottom-40px padding-bottom-lg-60px carousel">
-        <CarouselProvider
-          naturalSlideWidth={530}
-          naturalSlideHeight={420}
-          visibleSlides={testimonialCount}
-          totalSlides={testamonials && testamonials.length}
-          infinite={true}
-          isIntrinsicHeight={true}
-          currentSlide={5}
-          isPlaying={true}
-        >
-          <Slider>
-            {testamonials &&
-              testamonials.map((item, index) => (
-                <Slide key={index} index={index}>
-                  <div className="qoute-card bg-white d-flex align-items-center relative">
-                    <div className="text-16px c-black">
-                      <ReactMarkdown source={item.text} />
-                      <p className="margin-none text-14px font-barlow">
-                        <strong>{item.author}</strong>
-                      </p>
-                    </div>
-                    <img
-                      className="margin-none d-none d-md-block"
-                      src={CarouselImage(index)}
-                      alt=""
-                    />
+    <div className="container padding-top-40px padding-top-lg-60px padding-bottom-40px padding-bottom-lg-60px carousel">
+      <CarouselProvider
+        naturalSlideWidth={530}
+        naturalSlideHeight={420}
+        visibleSlides={testimonialCount}
+        totalSlides={testamonials && testamonials.length}
+        infinite={true}
+        isIntrinsicHeight={true}
+        currentSlide={5}
+        isPlaying={true}
+      >
+        <Slider>
+          {testamonials &&
+            testamonials.map((item, index) => (
+              <Slide key={index} index={index}>
+                <div className="qoute-card bg-white d-flex align-items-center relative">
+                  <div className="text-16px c-black">
+                    <ReactMarkdown source={item.text} />
+                    <p className="margin-none text-14px font-barlow">
+                      <strong>{item.author}</strong>
+                    </p>
                   </div>
-                </Slide>
-              ))}
-          </Slider>
-          <div className="container padding-top-50px padding-bottom-none padding-bottom-md-40px carousel-buttons">
-            <div className="d-flex justify-content-center justify-content-md-between align-items-center">
-              <ButtonBack className="carousel-prev-btn margin-left-5px d-flex">
-                <img className="margin-none" src={ArrowBack} alt="" />
-              </ButtonBack>
+                  <img
+                    className="margin-none d-none d-md-block"
+                    src={CarouselImage(index)}
+                    alt=""
+                  />
+                </div>
+              </Slide>
+            ))}
+        </Slider>
+        <div className="container padding-top-50px padding-bottom-none padding-bottom-md-40px carousel-buttons">
+          <div className="d-flex justify-content-center justify-content-md-between align-items-center">
+            <ButtonBack className="carousel-prev-btn margin-left-5px d-flex">
+              <img className="margin-none" src={ArrowBack} alt="" />
+            </ButtonBack>
 
-              <DotGroup
-                showAsSelectedForCurrentSlideOnly={true}
-                className="carousel-dots"
-              />
-              <ButtonNext className="carousel-next-btn d-flex">
-                <img className="margin-none" src={ArrowForward} alt="" />
-              </ButtonNext>
-            </div>
+            <DotGroup
+              showAsSelectedForCurrentSlideOnly={true}
+              className="carousel-dots"
+            />
+            <ButtonNext className="carousel-next-btn d-flex">
+              <img className="margin-none" src={ArrowForward} alt="" />
+            </ButtonNext>
           </div>
-        </CarouselProvider>
-      </div>
-    </>
+        </div>
+      </CarouselProvider>
+    </div>
   )
 }
 
